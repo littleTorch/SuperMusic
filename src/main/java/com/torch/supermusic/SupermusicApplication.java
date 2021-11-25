ed9bdb4ad9b9bf913f4ca80970224cedc8a7ffd2
小小炬 <1444504585@qq.com>
2021-11-25 19:27:40 +0800

    create

---
+++ src/main/java/com/torch/supermusic/SupermusicApplication.java
@@ -0,0 +1,13 @@
+package com.torch.supermusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/torch/supermusic/SupermusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torch.supermusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
